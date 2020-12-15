3b81f91d47e5385485209b3eefdc00ed3c4b1f42
Jon Cram <webignition@gmail.com>
2020-12-15 15:38:17 +0000

    Remove end to end test verifying http callback delays (#25)

--- tests/Integration/Asynchronous/EndToEnd/CompileExecuteTest.php
+++ tests/Integration/Asynchronous/EndToEnd/CompileExecuteTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Tests\Integration\Asynchronous\EndToEnd;
 
-use App\Model\BackoffStrategy\ExponentialBackoffStrategy;
 use App\Services\ApplicationState;
 use App\Services\CompilationState;
 use App\Services\ExecutionState;
@@ -79,54 +78,6 @@ class CompileExecuteTest extends AbstractEndToEndTest
                     Test::STATE_COMPLETE,
                     Test::STATE_COMPLETE,
                     Test::STATE_COMPLETE,
-                ]),
-            ],
-            'verify retried transactions are delayed' => [
-                'jobSetup' => (new JobSetup())
-                    ->withCallbackUrl('http://200.500.500.200.example.com/callback/2')
-                    ->withManifestPath(getcwd() . '/tests/Fixtures/Manifest/manifest-chrome-open-index.txt'),
-                'expectedSourcePaths' => [
-                    'Test/chrome-open-index.yml',
-                ],
-                'expectedCompilationEndState' => CompilationState::STATE_COMPLETE,
-                'expectedExecutionEndState' => ExecutionState::STATE_COMPLETE,
-                'expectedApplicationEndState' => ApplicationState::STATE_COMPLETE,
-                'assertions' => new InvokableCollection([
-                    'verify test end states' => TestGetterFactory::assertStates([
-                        Test::STATE_COMPLETE,
-                    ]),
-                    'verify http transactions' => new Invokable(
-                        function (HttpLogReader $httpLogReader) {
-                            $httpTransactions = $httpLogReader->getTransactions();
-                            $httpLogReader->reset();
-
-                            self::assertCount(4, $httpTransactions);
-
-                            $transactionPeriods = $httpTransactions->getPeriods()->getPeriodsInMicroseconds();
-                            array_shift($transactionPeriods);
-
-                            self::assertCount(3, $transactionPeriods);
-
-                            $firstStepTransactionPeriod = array_shift($transactionPeriods);
-                            $retriedTransactionPeriods = [];
-                            foreach ($transactionPeriods as $transactionPeriod) {
-                                $retriedTransactionPeriods[] = $transactionPeriod - $firstStepTransactionPeriod;
-                            }
-
-                            $backoffStrategy = new ExponentialBackoffStrategy();
-                            foreach ($retriedTransactionPeriods as $retryIndex => $retriedTransactionPeriod) {
-                                $retryCount = $retryIndex + 1;
-                                $expectedLowerThreshold = $backoffStrategy->getDelay($retryCount) * 1000;
-                                $expectedUpperThreshold = $backoffStrategy->getDelay($retryCount + 1) * 1000;
-
-                                self::assertGreaterThanOrEqual($expectedLowerThreshold, $retriedTransactionPeriod);
-                                self::assertLessThan($expectedUpperThreshold, $retriedTransactionPeriod);
-                            }
-                        },
-                        [
-                            new ServiceReference(HttpLogReader::class),
-                        ]
-                    )
                 ]),
             ],
             'verify job is timed out' => [
