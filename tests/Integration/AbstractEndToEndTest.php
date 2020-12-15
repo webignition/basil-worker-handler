1d729e0e2a41090edf19537ed25d9caef3330e49
Jon Cram <webignition@gmail.com>
2020-12-15 15:14:58 +0000

    Tidy up state assertions

--- tests/Integration/AbstractEndToEndTest.php
+++ tests/Integration/AbstractEndToEndTest.php
@@ -9,21 +9,18 @@ use App\Services\ApplicationState;
 use App\Services\CompilationState;
 use App\Services\ExecutionState;
 use App\Tests\Model\EndToEndJob\InvokableInterface;
-use App\Tests\Services\EntityRefresher;
-use App\Tests\Services\InvokableFactory\ApplicationStateGetterFactory;
-use App\Tests\Services\InvokableFactory\CompilationStateGetterFactory;
-use App\Tests\Services\InvokableFactory\ExecutionStateGetterFactory;
+use App\Tests\Services\InvokableFactory\ApplicationStateAsserter;
+use App\Tests\Services\InvokableFactory\CompilationStateAsserter;
+use App\Tests\Services\InvokableFactory\ExecutionStateAsserter;
 use App\Tests\Services\InvokableFactory\HttpLogResetter;
 use App\Tests\Services\InvokableFactory\JobSetup;
 use App\Tests\Services\InvokableFactory\JobSetupInvokableFactory;
 use App\Tests\Services\InvokableFactory\SourceCreationFactory;
 use App\Tests\Services\InvokableFactory\SourceGetterFactory;
+use App\Tests\Services\InvokableFactory\WaitUntilApplicationStateIs;
 use App\Tests\Services\InvokableHandler;
 use SebastianBergmann\Timer\Timer;
 use Symfony\Component\Messenger\MessageBusInterface;
-use webignition\BasilWorker\PersistenceBundle\Entity\Callback\CallbackEntity;
-use webignition\BasilWorker\PersistenceBundle\Entity\Job;
-use webignition\BasilWorker\PersistenceBundle\Entity\Test;
 use webignition\SymfonyTestServiceInjectorTrait\TestClassServicePropertyInjectorTrait;
 
 abstract class AbstractEndToEndTest extends AbstractBaseIntegrationTest
@@ -33,9 +30,7 @@ abstract class AbstractEndToEndTest extends AbstractBaseIntegrationTest
     private const MAX_DURATION_IN_SECONDS = 30;
     private const MICROSECONDS_PER_SECOND = 1000000;
 
-    protected EntityRefresher $entityRefresher;
     protected InvokableHandler $invokableHandler;
-    protected ApplicationState $applicationState;
     protected MessageBusInterface $messageBus;
 
     protected function setUp(): void
@@ -69,11 +64,8 @@ abstract class AbstractEndToEndTest extends AbstractBaseIntegrationTest
     ): void {
         $this->invokableHandler->invoke(JobSetupInvokableFactory::setup($jobSetup));
 
-        $compilationState = $this->invokableHandler->invoke(CompilationStateGetterFactory::get());
-        self::assertSame(CompilationState::STATE_AWAITING, $compilationState);
-
-        $executionState = $this->invokableHandler->invoke(ExecutionStateGetterFactory::get());
-        self::assertSame(ExecutionState::STATE_AWAITING, $executionState);
+        $this->invokableHandler->invoke(CompilationStateAsserter::is(CompilationState::STATE_AWAITING));
+        $this->invokableHandler->invoke(ExecutionStateAsserter::is(ExecutionState::STATE_AWAITING));
 
         $this->invokableHandler->invoke(SourceCreationFactory::createFromManifestPath($jobSetup->getManifestPath()));
 
@@ -85,58 +77,19 @@ abstract class AbstractEndToEndTest extends AbstractBaseIntegrationTest
         $sourcePaths = $this->invokableHandler->invoke(SourceGetterFactory::getAllRelativePaths());
         self::assertSame($expectedSourcePaths, $sourcePaths);
 
-        $this->waitUntilApplicationWorkflowIsComplete();
+        $this->invokableHandler->invoke(WaitUntilApplicationStateIs::create([
+            ApplicationState::STATE_COMPLETE,
+            ApplicationState::STATE_TIMED_OUT,
+        ]));
 
         $duration = $timer->stop();
 
-        self::assertSame(
-            $expectedCompilationEndState,
-            $this->invokableHandler->invoke(CompilationStateGetterFactory::get())
-        );
-
-        self::assertSame(
-            $expectedExecutionEndState,
-            $this->invokableHandler->invoke(ExecutionStateGetterFactory::get())
-        );
-
-        self::assertSame(
-            $expectedApplicationEndState,
-            $this->invokableHandler->invoke(ApplicationStateGetterFactory::get())
-        );
+        $this->invokableHandler->invoke(CompilationStateAsserter::is($expectedCompilationEndState));
+        $this->invokableHandler->invoke(ExecutionStateAsserter::is($expectedExecutionEndState));
+        $this->invokableHandler->invoke(ApplicationStateAsserter::is($expectedApplicationEndState));
 
         $this->invokableHandler->invoke($postAssertions);
 
         self::assertLessThanOrEqual(self::MAX_DURATION_IN_SECONDS, $duration->asSeconds());
-    }
-
-    private function waitUntilApplicationWorkflowIsComplete(): bool
-    {
-        $duration = 0;
-        $maxDuration = self::MAX_DURATION_IN_SECONDS * self::MICROSECONDS_PER_SECOND;
-        $maxDurationReached = false;
-        $intervalInMicroseconds = 100000;
-
-        $applicationFinishedStates = [ApplicationState::STATE_COMPLETE, ApplicationState::STATE_TIMED_OUT];
-
-        while (
-            false === $this->applicationState->is(...$applicationFinishedStates) &&
-            false === $maxDurationReached
-        ) {
-            usleep($intervalInMicroseconds);
-            $duration += $intervalInMicroseconds;
-            $maxDurationReached = $duration >= $maxDuration;
-
-            if ($maxDurationReached) {
-                return false;
-            }
-
-            $this->entityRefresher->refreshForEntities([
-                Job::class,
-                Test::class,
-                CallbackEntity::class,
-            ]);
-        }
-
-        return true;
     }
 }
---
+++ tests/Services/InvokableFactory/CompilationStateAsserter.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Services\InvokableFactory;
+
+use App\Services\CompilationState;
+use App\Tests\Model\EndToEndJob\Invokable;
+use App\Tests\Model\EndToEndJob\InvokableInterface;
+use App\Tests\Model\EndToEndJob\ServiceReference;
+use PHPUnit\Framework\TestCase;
+
+class CompilationStateAsserter
+{
+    /**
+     * @param CompilationState::STATE_* $state
+     */
+    public static function is(string $expectedState): InvokableInterface
+    {
+        return new Invokable(
+            function (CompilationState $compilationState, string $expectedState): void {
+                TestCase::assertSame($expectedState, $compilationState->getCurrentState());
+            },
+            [
+                new ServiceReference(CompilationState::class),
+                $expectedState,
+            ]
+        );
+    }
+}
