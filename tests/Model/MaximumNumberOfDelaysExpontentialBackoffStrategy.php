8621f6245499c7d26830074d8dee1336b41af1f2
Jon Cram <webignition@gmail.com>
2020-12-16 12:42:22 +0000

    Reduce delay time during testing

---
+++ tests/Model/MaximumNumberOfDelaysExpontentialBackoffStrategy.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Model;
+
+use App\Model\BackoffStrategy\ExponentialBackoffStrategy;
+
+class MaximumNumberOfDelaysExpontentialBackoffStrategy extends ExponentialBackoffStrategy
+{
+    private int $maximumNumberOfDelays;
+
+    public function __construct(int $maximumNumberOfDelays, int $window = 1000)
+    {
+        parent::__construct($window);
+
+        $this->maximumNumberOfDelays = $maximumNumberOfDelays;
+    }
+
+    public function getDelay(int $retryCount): int
+    {
+        if ($retryCount < $this->maximumNumberOfDelays) {
+            return parent::getDelay($retryCount);
+        }
+
+        return 0;
+    }
+}
