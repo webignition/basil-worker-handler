d36ee009403da312c1101b77f2da8c2f660fd801
Jon Cram <webignition@gmail.com>
2021-02-26 14:46:39 +0000

    Re-implement TestExecuteDocumentReceivedEvent handling

--- src/Event/TestExecuteDocumentReceivedEvent.php
+++ src/Event/TestExecuteDocumentReceivedEvent.php
@@ -4,28 +4,18 @@ declare(strict_types=1);
 
 namespace App\Event;
 
-use App\Model\Callback\ExecuteDocumentReceivedCallback;
-use webignition\BasilWorker\PersistenceBundle\Entity\Callback\CallbackInterface;
 use webignition\BasilWorker\PersistenceBundle\Entity\Test;
 use webignition\YamlDocument\Document;
 
-class TestExecuteDocumentReceivedEvent extends AbstractTestEvent implements CallbackEventInterface
+class TestExecuteDocumentReceivedEvent extends AbstractTestEvent
 {
-    public function __construct(
-        Test $test,
-        private Document $document,
-        private ExecuteDocumentReceivedCallback $callback
-    ) {
+    public function __construct(Test $test, private Document $document)
+    {
         parent::__construct($test);
     }
 
     public function getDocument(): Document
     {
         return $this->document;
-    }
-
-    public function getCallback(): CallbackInterface
-    {
-        return $this->callback;
     }
 }
