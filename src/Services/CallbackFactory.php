c12745aa5d1c3d8819c256f159b264b0203996a2
Jon Cram <webignition@gmail.com>
2021-02-26 16:21:58 +0000

    Create CallbackFactory

---
+++ src/Services/CallbackFactory.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Event\CallbackHttpErrorEvent;
+use App\Event\JobCompleteEvent;
+use App\Event\JobTimeoutEvent;
+use App\Event\SourceCompile\SourceCompileFailureEvent;
+use App\Event\TestExecuteDocumentReceivedEvent;
+use Symfony\Contracts\EventDispatcher\Event;
+use webignition\BasilWorker\PersistenceBundle\Entity\Callback\CallbackInterface;
+use webignition\BasilWorker\PersistenceBundle\Services\Factory\CallbackFactory as PersistenceBundleCallbackFactory;
+
+class CallbackFactory
+{
+    public function __construct(
+        private PersistenceBundleCallbackFactory $persistenceBundleCallbackFactory
+    ) {
+    }
+
+    public function createForEvent(Event $event): ?CallbackInterface
+    {
+        if ($event instanceof SourceCompileFailureEvent) {
+            return $this->persistenceBundleCallbackFactory->create(
+                CallbackInterface::TYPE_COMPILE_FAILURE,
+                $event->getOutput()->getData()
+            );
+        }
+
+        if ($event instanceof CallbackHttpErrorEvent) {
+            return $event->getCallback();
+        }
+
+        if ($event instanceof TestExecuteDocumentReceivedEvent) {
+            $document = $event->getDocument();
+
+            $documentData = $document->parse();
+            $documentData = is_array($documentData) ? $documentData : [];
+
+            return $this->persistenceBundleCallbackFactory->create(
+                CallbackInterface::TYPE_EXECUTE_DOCUMENT_RECEIVED,
+                $documentData
+            );
+        }
+
+        if ($event instanceof JobTimeoutEvent) {
+            return $this->persistenceBundleCallbackFactory->create(CallbackInterface::TYPE_JOB_TIMEOUT, []);
+        }
+
+        if ($event instanceof JobCompleteEvent) {
+            return $this->persistenceBundleCallbackFactory->create(CallbackInterface::TYPE_JOB_COMPLETE, []);
+        }
+
+        return null;
+    }
+}
