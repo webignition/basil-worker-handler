2c9a5464371f080d92ef525ba9969cedb154fbb0
Jon Cram <webignition@gmail.com>
2021-02-26 13:52:01 +0000

    Re-implement JobTimeoutEvent handling (#114)

--- src/Event/JobTimeoutEvent.php
+++ src/Event/JobTimeoutEvent.php
@@ -4,26 +4,16 @@ declare(strict_types=1);
 
 namespace App\Event;
 
-use App\Model\Callback\JobTimeoutCallback;
 use Symfony\Contracts\EventDispatcher\Event;
-use webignition\BasilWorker\PersistenceBundle\Entity\Callback\CallbackInterface;
 
-class JobTimeoutEvent extends Event implements CallbackEventInterface
+class JobTimeoutEvent extends Event
 {
-    private CallbackInterface $callback;
-
     public function __construct(private int $jobMaximumDuration)
     {
-        $this->callback = new JobTimeoutCallback($this->jobMaximumDuration);
     }
 
     public function getJobMaximumDuration(): int
     {
         return $this->jobMaximumDuration;
-    }
-
-    public function getCallback(): CallbackInterface
-    {
-        return $this->callback;
     }
 }
