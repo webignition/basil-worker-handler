dca4168aa0841c59abae0cee48e42b5c528dab79
Jon Cram <webignition@gmail.com>
2021-03-02 16:40:43 +0000

    Create compilation/started callback

---
+++ src/Services/EventCallbackFactory/CompilationStartedEventCallbackFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\EventCallbackFactory;
+
+use App\Event\SourceCompilation\SourceCompilationStartedEvent;
+use Symfony\Contracts\EventDispatcher\Event;
+use webignition\BasilWorker\PersistenceBundle\Entity\Callback\CallbackInterface;
+
+class CompilationStartedEventCallbackFactory extends AbstractEventCallbackFactory
+{
+    public function handles(Event $event): bool
+    {
+        return $event instanceof SourceCompilationStartedEvent;
+    }
+
+    public function createForEvent(Event $event): ?CallbackInterface
+    {
+        if ($event instanceof SourceCompilationStartedEvent) {
+            return $this->create(
+                CallbackInterface::TYPE_COMPILATION_STARTED,
+                [
+                    'source' => $event->getSource(),
+                ]
+            );
+        }
+
+        return null;
+    }
+}
