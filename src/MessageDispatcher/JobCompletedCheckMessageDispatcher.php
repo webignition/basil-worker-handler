01b4d8a8c5f9ad433c70017df5d1921ccee51602
Jon Cram <webignition@gmail.com>
2021-03-08 16:08:58 +0000

    Create message dispatcher

---
+++ src/MessageDispatcher/JobCompletedCheckMessageDispatcher.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MessageDispatcher;
+
+use App\Message\JobCompleteCheckMessage;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\DelayStamp;
+
+class JobCompletedCheckMessageDispatcher
+{
+    public const MILLISECONDS_PER_SECOND = 1000;
+
+    public function __construct(
+        private MessageBusInterface $messageBus,
+        private int $recheckPeriodInSeconds,
+        private bool $enabled = true
+    ) {
+    }
+
+    public function dispatch(): void
+    {
+        $message = new JobCompleteCheckMessage();
+        $envelope = new Envelope($message, [
+            new DelayStamp($this->recheckPeriodInSeconds * self::MILLISECONDS_PER_SECOND)
+        ]);
+
+        if ($this->enabled) {
+            $this->messageBus->dispatch($envelope);
+        }
+    }
+}
