fa343d393db98a9d909b56cbf8a86247b049b52f
Jon Cram <webignition@gmail.com>
2021-03-08 15:40:00 +0000

    Create EventListenerRemover, implement poc

---
+++ tests/Services/EventListenerRemover.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Services;
+
+use Psr\Container\ContainerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class EventListenerRemover
+{
+    public function __construct(
+        private ContainerInterface $container,
+        private EventDispatcherInterface $eventDispatcher,
+    ) {
+    }
+
+    /**
+     * @param class-string $serviceId
+     * @param class-string $eventClass
+     * @param string $methodName
+     */
+    public function removeServiceMethodForEvent(string $serviceId, string $eventClass, string $methodName): void
+    {
+        $service = $this->container->get($serviceId);
+        if (is_object($service)) {
+            $callable = [$service, $methodName];
+            if (is_callable($callable)) {
+                $this->eventDispatcher->removeListener($eventClass, $callable);
+            }
+        }
+    }
+
+    /**
+     * @param class-string $serviceId
+     * @param array<class-string, string> $eventsAndMethods
+     */
+    public function removeServiceMethodsForEvents(string $serviceId, array $eventsAndMethods): void
+    {
+        foreach ($eventsAndMethods as $eventClass => $methodName) {
+            $this->removeServiceMethodForEvent($serviceId, $eventClass, $methodName);
+        }
+    }
+}
