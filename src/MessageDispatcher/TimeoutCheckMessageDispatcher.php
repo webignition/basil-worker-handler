0b30d599fc95cdf942385bb787eb4e614c4d5b39
Jon Cram <webignition@gmail.com>
2021-04-08 14:45:52 +0100

    Remove JobCompletedCheckMessageDispatcher (#233)

--- src/MessageDispatcher/TimeoutCheckMessageDispatcher.php
+++ src/MessageDispatcher/TimeoutCheckMessageDispatcher.php
@@ -7,9 +7,21 @@ namespace App\MessageDispatcher;
 use App\Event\JobReadyEvent;
 use App\Message\TimeoutCheckMessage;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\DelayStamp;
 
-class TimeoutCheckMessageDispatcher extends AbstractDelayedMessageDispatcher implements EventSubscriberInterface
+class TimeoutCheckMessageDispatcher implements EventSubscriberInterface
 {
+    public const MILLISECONDS_PER_SECOND = 1000;
+
+    public function __construct(
+        private MessageBusInterface $messageBus,
+        private int $recheckPeriodInSeconds,
+        private bool $enabled = true
+    ) {
+    }
+
     public static function getSubscribedEvents()
     {
         return [
@@ -19,8 +31,16 @@ class TimeoutCheckMessageDispatcher extends AbstractDelayedMessageDispatcher imp
         ];
     }
 
-    protected function createMessage(): object
+    public function dispatch(): void
     {
-        return new TimeoutCheckMessage();
+        if ($this->enabled) {
+            $message = new TimeoutCheckMessage();
+            $envelope = new Envelope($message, [
+                new DelayStamp($this->recheckPeriodInSeconds * self::MILLISECONDS_PER_SECOND)
+            ]);
+
+
+            $this->messageBus->dispatch($envelope);
+        }
     }
 }
